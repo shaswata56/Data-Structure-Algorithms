4bbf34ba5f976c6dc91afc0aa9acc23dc6b42343
Shaswata Das <shaswata56@gmail.com>
2018-09-24 19:11:15 +0600

    Update Remove.c

--- implementation/string/Remove.c
+++ implementation/string/Remove.c
@@ -10,7 +10,7 @@ void Remove(char *str, int position, int len)
 int main(void)
 {
   char str[100] = "a quick brown fox jumps over the lazy dog.", len = 5, position=33;
-  printf("\t::This program insert a substring in a string::\n");
+  printf("\t::This program remove substring from a string::\n");
   printf("\nOriginal string was: %s\n", str);
   Remove(str, position, len);
   printf("\nNow the string is: %s\n", str);
