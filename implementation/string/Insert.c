b4d1184293c52b620814b3342bd1d0e327efb12b
Shaswata Das <shaswata56@gmail.com>
2018-09-24 19:10:20 +0600

    Create Insert.c

---
+++ implementation/string/Insert.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void insert(char *str, int position, char *substr)
+{
+  int strlen=0, sstrlen=0, i;
+  while(str[strlen] != '\0') strlen++;
+  while(substr[sstrlen] != '\0') sstrlen++;
+  for(i=strlen; i>=position; i--) str[i+sstrlen]=str[i];
+  for(i=position; i<position+sstrlen; i++) str[i]=substr[i-position];
+}
+
+int main(void)
+{
+  char str[100] = "a quick brown fox jumps over the  dog.", substr[100] = "lazy", position=33;
+  printf("\t::This program insert a substring in a string::\n");
+  printf("\nOriginal string was: %s\n", str);
+  insert(str, position, substr);
+  printf("\nNow the string is: %s\n", str);
+  return 0;
+}
