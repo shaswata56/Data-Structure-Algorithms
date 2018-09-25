e0142bc362ad6c1417a76cbe1230bf53ea902d54
shaswata56 <shaswata56@gmail.com>
2018-09-25 22:58:14 +0600

    complete

--- Chapter-3-String-Processing/BubbleSort.c
+++ Chapter-3-String-Processing/BubbleSort.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+void swap(int* a, int* b)
+{
+    int t=*a;
+    *a= *b;
+    *b= t;
+}
+
+void bubblesort(int arr[], int n)
+{
+    if (n == 1)
+        return;
+    for (int i=0; i<n-1; i++)
+        if (arr[i] > arr[i+1])
+            swap(&arr[i], &arr[i+1]);
+    bubblesort(arr, n-1);
+}
+
 void printArray(int arr[], int size)
 {
     int i;
     for(i=0; i <size ; i++)
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
@@ -12,7 +29,9 @@ int main()
 {
     int arr[] = {10,7,8,9,1,5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    bubblesort(arr, 0 ,n-1);
+    printf("Ordinary Array: ");
+    printArray(arr,n);
+    bubblesort(arr, n);
     printf("Sorted Array: ");
     printArray(arr,n);
     return 0;
